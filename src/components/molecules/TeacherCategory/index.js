273d5f6236b884863f20dadd77ee671a37889005
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-01-16 16:45:18 +0700

    doctor page - category doctor _section label

--- src/components/molecules/TeacherCategory/index.js
+++ src/components/molecules/TeacherCategory/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {ILCatUmum} from '../../../assets';
+import {colors, fonts} from '../../../utils';
 
 const TeacherCategory = () => {
   return (
-    <View>
-      <Text>Teacher Category Component</Text>
+    <View style={styles.container}>
+      <ILCatUmum style={styles.illustration} />
+      <Text style={styles.label}>guru</Text>
+      <Text style={styles.category}>matematika</Text>
     </View>
   );
 };
 
 export default TeacherCategory;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 12,
+    backgroundColor: colors.cardLight,
+    alignSelf: 'flex-start',
+    borderRadius: 10,
+    marginRight: 10,
+    width: 100,
+    height: 130,
+  },
+  illustration: {marginBottom: 28},
+  label: {
+    fontSize: 12,
+    fontFamily: fonts.primary[300],
+    color: colors.text.primary,
+  },
+  category: {
+    fontSize: 12,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+  },
+});
--- src/pages/Teacher/index.js
+++ src/pages/Teacher/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {
   TeacherCategory,
+  Gap,
   HomeProfile,
   NewsItem,
   RatedTeacher,
@@ -11,20 +12,32 @@ import {colors, fonts} from '../../utils';
 const Teacher = () => {
   return (
     <View style={styles.page}>
-      <HomeProfile />
-      <Text style={styles.welcome}>Mau konsultasi dengan siapa hari ini?</Text>
-      <TeacherCategory />
-      <TeacherCategory />
-      <TeacherCategory />
-      <TeacherCategory />
-      <Text>Top Rated Teachers</Text>
-      <RatedTeacher />
-      <RatedTeacher />
-      <RatedTeacher />
-      <Text>Good News</Text>
-      <NewsItem />
-      <NewsItem />
-      <NewsItem />
+      <View style={styles.content}>
+        <HomeProfile />
+        <Text style={styles.welcome}>
+          Mau konsultasi dengan siapa hari ini?
+        </Text>
+        <View style={styles.wrapperScroll}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View style={styles.category}>
+              <Gap width={16} />
+              <TeacherCategory />
+              <TeacherCategory />
+              <TeacherCategory />
+              <TeacherCategory />
+              <Gap width={6} />
+            </View>
+          </ScrollView>
+        </View>
+        <Text style={styles.sectionLabel}>Top Rated Teachers</Text>
+        <RatedTeacher />
+        <RatedTeacher />
+        <RatedTeacher />
+        <Text style={styles.sectionLabel}>Good News</Text>
+        <NewsItem />
+        <NewsItem />
+        <NewsItem />
+      </View>
     </View>
   );
 };
@@ -32,7 +45,18 @@ const Teacher = () => {
 export default Teacher;
 
 const styles = StyleSheet.create({
-  page: {paddingVertical: 30, paddingHorizontal: 16},
+  page: {
+    backgroundColor: colors.secondary,
+    flex: 1,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    paddingVertical: 30,
+    paddingHorizontal: 16,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
   welcome: {
     fontSize: 20,
     fontFamily: fonts.primary[600],
@@ -40,5 +64,14 @@ const styles = StyleSheet.create({
     marginTop: 30,
     marginBottom: 16,
     maxWidth: 209,
+  },
+  category: {flexDirection: 'row'},
+  wrapperScroll: {marginHorizontal: -16},
+  sectionLabel: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginBottom: 16,
   },
 });
