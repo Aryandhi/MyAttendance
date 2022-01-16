5776a64c6d53d19b7490028237707356201c9d6a
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-01-16 16:17:59 +0700

    custom bottom tab navigator

--- src/utils/colors/index.js
+++ src/utils/colors/index.js
@@ -1,10 +1,10 @@
 const mainColors = {
   green1: '#0bcad4',
   dark1: '#112340',
+  dark2: '#495a75',
   grey1: '#7d8797',
   grey2: '#e9e9e9',
 };
-
 export const colors = {
   primary: mainColors.green1,
   secondary: mainColors.dark1,
@@ -13,6 +13,8 @@ export const colors = {
   text: {
     primary: mainColors.dark1,
     secondary: mainColors.grey1,
+    menuInactive: mainColors.dark2,
+    menuActive: mainColors.green1,
   },
   button: {
     primary: {
