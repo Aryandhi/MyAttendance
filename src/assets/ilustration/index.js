f34b072a7fff1bf0a3a532670e1556d3e9f170bd
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-01-16 08:49:37 +0700

    created splash screen,  GetStarted dan Custom font

--- src/assets/ilustration/index.js
+++
@@ -1,7 +0,0 @@
-import ILLogo from './logo.svg';
-import ILGetStarted from './started.png';
-import ILNullPhoto from './null-photo.png';
-import ILCatUmum from './cat-dok-umum.svg';
-import ILSMKBG from './smk2mei-background.png';
-
-export {ILLogo, ILGetStarted, ILNullPhoto, ILCatUmum, ILSMKBG};
