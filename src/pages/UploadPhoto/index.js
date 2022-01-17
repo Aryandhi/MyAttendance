ce40379fad1825e418f53f9a5edfb1f88e54c48b
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-01-17 10:28:05 +0700

    menyiapkan halaman upload photo

--- src/pages/UploadPhoto/index.js
+++ src/pages/UploadPhoto/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View, Image} from 'react-native';
-import {IconAddPhoto, ILNullPhoto} from '../../assets';
+import {IconAddPhoto, IconRemovePhoto, ILNullPhoto} from '../../assets';
 import {Button, Gap, Header, Link} from '../../components';
 import {colors, fonts} from '../../utils';
 
 const UploadPhoto = ({navigation}) => {
+  const [hasPhoto, setHasPhoto] = useState(false);
   return (
     <View style={styles.page}>
       <Header title="Upload Photo" onPress={() => navigation.goBack()} />
@@ -12,13 +13,15 @@ const UploadPhoto = ({navigation}) => {
         <View style={styles.profile}>
           <View style={styles.avatarWrapper}>
             <Image source={ILNullPhoto} style={styles.avatar} />
-            <IconAddPhoto style={styles.addPhoto} />
+            {hasPhoto && <IconRemovePhoto style={styles.addPhoto} />}
+            {!hasPhoto && <IconAddPhoto style={styles.addPhoto} />}
           </View>
           <Text style={styles.name}>Ardi Wibowo</Text>
           <Text style={styles.profession}>Product Designer</Text>
         </View>
         <View>
           <Button
+            disable
             title="Upload and Continue"
             onPress={() => navigation.replace('MainApp')}
           />
