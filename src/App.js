444cfe52d378665771a1e2cbfcc622bc1f3ee401
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-01-17 15:28:22 +0700

    install redux dan react-redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import Router from './router';
+import React, {useState} from 'react';
 import FlashMessage from 'react-native-flash-message';
+import {Provider} from 'react-redux';
+import {Loading} from './components';
+import store from './redux/store';
+import Router from './router';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
   return (
-    <>
+    <Provider store={store}>
       <NavigationContainer>
         <Router />
       </NavigationContainer>
       <FlashMessage position="top" />
-    </>
+    </Provider>
   );
 };
 export default App;
