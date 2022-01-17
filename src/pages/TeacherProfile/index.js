afb0b33dea92641bd035cc7ce42294b40484807f
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-01-17 14:57:11 +0700

    edit teacher profile - dan photo belum tampil

--- src/pages/TeacherProfile/index.js
+++ src/pages/TeacherProfile/index.js
@@ -6,10 +6,10 @@ import {colors} from '../../utils';
 const TeacherProfile = ({navigation}) => {
   return (
     <View style={styles.page}>
-      <Header title="Doctor Profile" onPress={() => navigation.goBack()} />
-      <Profile name="Hendro Junawarko Spd" desc="Matematika" />
+      <Header title="Teacher Profile" onPress={() => navigation.goBack()} />
+      <Profile name="Hendro Junawarko S.Kom" desc="Matematika" />
       <Gap height={10} />
-      <ProfileItem label="Alumnus" value="Universitas Indonesia, 2015" />
+      <ProfileItem label="Alumnus" value="Universitas Teknokrat, 2023" />
       <ProfileItem label="SMK 2 Mei" value="Jl ZA Pagar Alam, Bandar Lampung" />
       <ProfileItem label="NIP." value="0000116622081996" />
       <View style={styles.action}>
