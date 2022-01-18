1ce447258e566b2dcf31d65f86c7e76b542d462c
WINDOWS-72MUN56\User <aryandh.a.w@gmail.com>
2022-01-18 12:47:03 +0700

    menghilangkan warning logBox dan hapus Dummysch

--- src/assets/dummy/index.js
+++ src/assets/dummy/index.js
@@ -5,8 +5,8 @@ import Guru2 from './guru2.png';
 import Guru3 from './guru3.png';
 import Guru4 from './guru4.png';
 import News1 from './news.png';
-import DummySch1 from './DummySch1.png';
-import DummySch2 from './DummySch2.png';
-import DummySch3 from './DummySch3.png';
+// import DummySch1 from './DummySch1.png';
+// import DummySch2 from './DummySch2.png';
+// import DummySch3 from './DummySch3.png';
 
-export {User, Guru1, Guru2, Guru3, Guru4, News1, DummySch1, DummySch2, DummySch3, User1};
+export {User, Guru1, Guru2, Guru3, Guru4, News1, User1};
